cc30a50e7e71158c746ec1a84345f196b1b86202
itramell <iona_tramelli@taylor.edu>
2022-11-20 15:52:07 -0500

    Added show.html.erb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Aritcle.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   mount Avo::Engine, at: Avo.configuration.root_path
   get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
