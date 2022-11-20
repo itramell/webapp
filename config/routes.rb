07fea8a0468d892b375d7326522ad4fb352b4278
itramell <iona_tramelli@taylor.edu>
2022-11-20 16:01:21 -0500

    Finished R of CRUD in article

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@
 Rails.application.routes.draw do
   resources :users
   mount Avo::Engine, at: Avo.configuration.root_path
-  get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
