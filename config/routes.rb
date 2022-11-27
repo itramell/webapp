9c7006f101d02766f458b2a1084cca91bc125d68
itramell <iona_tramelli@taylor.edu>
2022-11-27 16:29:08 -0500

    Added comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@
 Rails.application.routes.draw do
   resources :users
   mount Avo::Engine, at: Avo.configuration.root_path
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
