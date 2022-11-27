a37bc99ef75291e3240fef9a41e071e4b705b1ed
itramell <iona_tramelli@taylor.edu>
2022-11-27 17:42:37 -0500

    Added concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,14 @@
 #
 #  id         :integer          not null, primary key
 #  body       :text
+#  status     :string
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Article < ApplicationRecord
+    include Visible
+
     has_many :comments
 
     validates :title, presence: true
